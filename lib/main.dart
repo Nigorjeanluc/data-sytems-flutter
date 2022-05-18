0b6b55c4f010e6cf0edd1acb8a802ff630faae80
Igor J.Luc Ndiramiye <nigorjeanluc@gmail.com>
2022-05-18 19:41:59 +0200

    Handle youtube video player

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,10 +11,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
+        theme: ThemeData(primaryColor: Colors.red[900]),
+        home: YoutubeVideo(videoId: 'dYZGnPuWmfQ'),
+    );
+  }
+}
+
+class YoutubeVideo extends StatefulWidget {
+  // ignore: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+  YoutubeVideo({this.videoId});
+  // ignore: prefer_typing_uninitialized_variables
+  final videoId;
+
+  @override
+  YoutubeVideoState createState() => YoutubeVideoState();
+}
+
+class YoutubeVideoState extends State<YoutubeVideo> {
+  late YoutubePlayerController _controller;
+
+  void runYoutubePlayer() {
+    _controller = YoutubePlayerController(
+        initialVideoId: widget.videoId,
+        flags: const YoutubePlayerFlags(
+          enableCaption: false,
+          isLive: false,
+          autoPlay: false,
+        ),
+    );
+  }
+
+  @override
+  void initState() {
+    runYoutubePlayer();
+    super.initState();
+  }
+
+  @override
+  // ignore: override_on_non_overriding_member
+  void deactive() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+        player: YoutubePlayer(
+          controller: _controller,
+            bottomActions: [
+              CurrentPosition(),
+              ProgressBar(isExpanded: true),
+              PlayPauseButton(),
+            ],
+        ),
+        builder: (content, player){
+          return Scaffold(
             appBar: AppBar(
-          title: const Text('Sample Code'),
-        )));
+              title: const Text('Youtube Video Player'),
+            ),
+            body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    player,
+                    SizedBox(height: 40.0,),
+                    Text(
+                        'Youtube Player'
+                    ),
+                  ]
+              ),
+          );
+        }
+    );
   }
 }
